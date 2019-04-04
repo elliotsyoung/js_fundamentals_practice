ad2e408971792109a17da448768426c92c5b1d5f
Justin <justin.christopher.russell@gmail.com>
2019-04-04 13:53:46 -0700

    mystuff needed to be overwritten

--- language_basics.js
+++ language_basics.js
@@ -1,3 +1,15 @@
 // There are a few unusual langauge features in JS:
 
-// 1. Variables
+// 1. Variables are hoisted to the top wherever they are scoped. when you use Variables not constant
+
+
+
+console.log(name);
+var name = "elliot";
+
+let obj = {
+  name: "justin",
+  age: 29
+}
+
+console.log(obj.last_name);
